72db3092572ccbfb4a5507ebf0b7466be1f77c92
Ties Westendorp <tieswestendorp@gmail.com>
2021-10-02 23:32:47 +0200

    Finish lint remarks for sequence model

--- models/sequence.py
+++ models/sequence.py
@@ -1,60 +1,66 @@
+"""
+Defines the Sequence class which provides a way to create generators of integers that
+iterate a finite list followed by a repeating pattern, as well as logic to compose
+them.
+"""
+
 from typing import List, Iterator
 from math import ceil
 from models.helpers import divisors, lcm
 
 class Sequence:
-    """Infinite sequence of integers consisting of a finite prefix and a repeating part"""
+    """Infinite sequence of integers consisting of a finite prefix and a repeating pattern"""
 
     zero = None
     one  = None
 
-    def __init__(self, prefix: List[int], repeat: List[int]) -> None:
+    def __init__(self, prefix: List[int], pattern: List[int]) -> None:
         self.prefix = prefix
-        self.repeat = repeat if len(repeat) > 0 else [0]
+        self.pattern = pattern if len(pattern) > 0 else [0]
         self.normalize()
 
     def normalize(self):
         """Normalizes the sequence, such that comparison can be done elementwise"""
-        # Reduce prefix size by 'rotating' the repeated part
-        while len(self.prefix) > 0 and self.prefix[-1] == self.repeat[-1]:
+        # Reduce prefix size by 'rotating' the repeated pattern
+        while len(self.prefix) > 0 and self.prefix[-1] == self.pattern[-1]:
             self.prefix = self.prefix[:-1]
-            self.repeat = self.repeat[-1:]+self.repeat[:-1]
+            self.pattern = self.pattern[-1:]+self.pattern[:-1]
 
-        # Reduce repeated size by identifying sublist that multiply
-        for divisor in divisors(len(self.repeat)):
-            times = len(self.repeat)//divisor
-            if self.repeat == times*self.repeat[:divisor]:
-                self.repeat = self.repeat[:divisor]
+        # Reduce pattern size by identifying sublist that multiply
+        for divisor in divisors(len(self.pattern)):
+            times = len(self.pattern)//divisor
+            if self.pattern == times*self.pattern[:divisor]:
+                self.pattern = self.pattern[:divisor]
                 break
         return self
 
     # Assumes the sequences are normalized
     def __eq__(self, other: 'Sequence') -> bool:
-        return self.prefix == other.prefix and self.repeat == other.repeat
+        return self.prefix == other.prefix and self.pattern == other.pattern
 
     def __add__(self, other: 'Sequence') -> 'Sequence':
         prefix_length = max([len(self.prefix), len(other.prefix)])
-        repeat_length = lcm(len(self.repeat), len(other.repeat))
-        prefix1 = self.finite_prefix(prefix_length + repeat_length)
-        prefix2 = other.finite_prefix(prefix_length + repeat_length)
+        pattern_length = lcm(len(self.pattern), len(other.pattern))
+        prefix1 = self.finite_prefix(prefix_length + pattern_length)
+        prefix2 = other.finite_prefix(prefix_length + pattern_length)
         summed = list(map(sum, zip(prefix1, prefix2)))
         return Sequence(summed[:prefix_length], summed[prefix_length:]).normalize()
 
     def prefixed_by(self, additional_prefix: List[int]) -> 'Sequence':
         """Prefix an additional number to the sequence"""
-        return Sequence(additional_prefix + self.prefix, self.repeat)
+        return Sequence(additional_prefix + self.prefix, self.pattern)
 
     def finite_prefix(self, length: int) -> List[int]:
         """Returns finite prefix of the sequence of the given length"""
-        k = ceil((length - len(self.prefix))/len(self.repeat))
-        return (self.prefix + k*self.repeat)[:length]
+        k = ceil((length - len(self.prefix))/len(self.pattern))
+        return (self.prefix + k*self.pattern)[:length]
 
     def generator(self) -> Iterator[int]:
         """Returns a generator that iterates the sequence"""
         for element in self.prefix:
             yield element
         while True:
-            for element in self.repeat:
+            for element in self.pattern:
                 yield element
 
     @staticmethod
