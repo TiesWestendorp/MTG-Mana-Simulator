5ee15c6115f65db28e76a53c88bc34e76dbe84b0
Ties Westendorp <tieswestendorp@gmail.com>
2021-10-03 20:06:54 +0200

    Rename agent back to ai

--- sanity_check_2.py
+++ sanity_check_2.py
@@ -7,7 +7,7 @@ from models import AI, Card, Experiment, Metric
 
 for n in range(50, 100, 10):
     deck = n*[Card.untapped_land] + 6*[Card.filler]
-    experiment = Experiment(deck=deck, agent=AI.naive, turns=8, repeats=1)
+    experiment = Experiment(deck=deck, ai=AI.naive, turns=8, repeats=1)
 
     results = list(experiment.evaluate([Metric.on_curve]).values())[0]
 
--- sanity_check_4.py
+++ sanity_check_4.py
@@ -9,8 +9,8 @@ seed(1337)
 
 deck1 = 38*[Card.untapped_land] + 61*[Card.filler]
 deck2 = 30*[Card.untapped_land] + 69*[Card.filler]
-experiment1 = Experiment(deck=deck1, agent=AI.naive, turns=8, repeats=5000)
-experiment2 = Experiment(deck=deck2, agent=AI.naive, turns=8, repeats=5000)
+experiment1 = Experiment(deck=deck1, ai=AI.naive, turns=8, repeats=5000)
+experiment2 = Experiment(deck=deck2, ai=AI.naive, turns=8, repeats=5000)
 
 results1 = list(experiment1.evaluate([Metric.mean]).values())[0]
 results2 = list(experiment2.evaluate([Metric.mean]).values())[0]
