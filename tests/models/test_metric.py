3fafdce03d3bdc347e8726938a9ec31a64b759ea
Ties Westendorp <tieswestendorp@gmail.com>
2021-10-13 13:12:51 +0200

    Fix tests

--- tests/models/test_metric.py
+++ tests/models/test_metric.py
@@ -3,6 +3,8 @@ Unit tests for Metric class
 """
 
 from models.metric import Metric
+from pytest import raises
+from statistics import StatisticsError
 
 def traces():
     trace1 = [1,2,3]
@@ -20,6 +22,11 @@ def test_compute():
         (2, [2,2,0,0]),
         (3, [3,2,1,0])]
     assert Metric.mean.compute(traces())        == [0.5, 1.0,  1.5]
+    assert Metric.median.compute(traces())      == [0, 1, 1]
+    with raises(StatisticsError):
+        Metric.mode.compute(traces())
+    #assert Metric.variance.compute(traces())    == [1/3, 1+1/3, 1+2/3]
+    assert Metric.below_curve.compute(traces()) == [0.5, 0.5, 0.75]
     assert Metric.on_curve.compute(traces())    == [0.5, 0.5, 0.25]
     assert Metric.above_curve.compute(traces()) == [0.0, 0.0, 0.0]
     assert Metric.minimum.compute(traces())     == [0, 0, 0]
@@ -29,10 +36,8 @@ def test_minimum_mana():
     """
     Test minimum_mana static method
     """
-    pass
 
 def test_percentile():
     """
     Test percentile static method
     """
-    pass
