97cb31807a4ad0919d2286e4cd1a85ebf37e23f7
Ties Westendorp <tieswestendorp@gmail.com>
2021-10-17 16:59:28 +0200

    Fix setup.py

--- mtg_mana_simulator/version.py
+++ mtg_mana_simulator/version.py
@@ -1,4 +1,4 @@
 """
 Defines the version in a separate file so it can be fetched by setup.py
 """
-__version__ = '0.1'
+__version__ = '0.2'
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='mtg-mana-simulator',
-    version='0.1',
+    version='0.2',
     url='https://github.com/TiesWestendorp/MTG-Mana-Simulator',
     author='Ties Westendorp',
-    py_modules=find_packages(include=['mtg_mana_simulator', 'mtg_mana_simulator.*']),
+    packages=find_packages(include=['mtg_mana_simulator', 'mtg_mana_simulator.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
