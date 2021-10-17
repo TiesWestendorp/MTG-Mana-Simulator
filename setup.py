65d87fed1b0f9848d763efb5586035954c5c4213
Ties Westendorp <tieswestendorp@gmail.com>
2021-10-17 16:43:20 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='0.1',
     url='https://github.com/TiesWestendorp/MTG-Mana-Simulator',
     author='Ties Westendorp',
-    py_modules=find_packages(),
+    py_modules=find_packages(include=['mtg_mana_simulator', 'mtg_mana_simulator.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
