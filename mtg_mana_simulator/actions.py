b83ba1197c8bdee8b8abfabc2b884e1412312ee2
Ties Westendorp <tieswestendorp@gmail.com>
2022-08-07 16:33:56 +0200

    Add basic to hand action

--- mtg_mana_simulator/actions.py
+++ mtg_mana_simulator/actions.py
@@ -1,4 +1,5 @@
-from random import choice, shuffle
+from typing import TYPE_CHECKING
+from random import shuffle
 if TYPE_CHECKING:
     from mtg_mana_simulator.context import Context
 
@@ -9,11 +10,22 @@ def basic_untapped_into_play(context: "Context") -> None:
     library = context.zones["deck"]
     indices = [index for index in range(len(library)) if library[index].name == "Basic land"]
     if len(indices) > 0:
-        context.play_card("deck", choice(indices))
+        context.play_card("deck", indices[0])
+        context.land += 1 # Searching a basic doesn't count as playing a land
+        shuffle_library(context)
 
 def basic_tapped_into_play(context: "Context") -> None:
-    # To do
     library = context.zones["deck"]
     indices = [index for index in range(len(library)) if library[index].name == "Basic land"]
     if len(indices) > 0:
-        context.play_card("deck", choice(indices))
+        context.play_card("deck", indices[0])
+        context.mana -= 1 # It came into play tapped, so didn't give mana this turn
+        context.land += 1 # Searching a basic doesn't count as playing a land
+        shuffle_library(context)
+
+def basic_to_hand(context: "Context") -> None:
+    library = context.zones["deck"]
+    indices = [index for index in range(len(library)) if library[index].name == "Basic land"]
+    if len(indices) > 0:
+        context.zones["hand"].append(library.pop(indices[0]))
+        shuffle_library(context)
